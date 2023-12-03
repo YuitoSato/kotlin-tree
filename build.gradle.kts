9b1a9af7e9f8c2dbaa4081fd211819b72b468766
YuitoSato <yuitosato.w@gmail.com>
2023-12-03 16:58:42 +0900

    wip: settings for kotlin multiplatform

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("multiplatform") version "1.9.20"
 
     id("org.jlleitschuh.gradle.ktlint") version "11.5.0"
     id("signing")
     id("maven-publish")
     id("io.codearte.nexus-staging") version "0.30.0"
+    id("io.kotest.multiplatform") version "5.8.0"
 }
 
 group = "io.github.yuitosato"
@@ -16,23 +17,50 @@ repositories {
     mavenCentral()
 }
 
-val kotestVersion = "5.6.2"
-
-dependencies {
-    testImplementation(kotlin("test"))
-    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
-    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
+val kotestVersion = "5.8.0"
 
 val jvmVersion = 11
 
 kotlin {
-    jvmToolchain {
-        (this).languageVersion.set(JavaLanguageVersion.of(jvmVersion))
+    jvmToolchain(jvmVersion)
+
+    jvm() // JVMターゲット
+    js(IR) { // JavaScriptターゲット（IRベース）
+        browser()
+        nodejs()
+    }
+//    androidTarget()
+    iosArm64()
+    iosSimulatorArm64()
+//    iosX64("ios") // iOSターゲット（ここではX64のみを例としています）
+    applyDefaultHierarchyTemplate()
+
+    sourceSets {
+        val commonMain by getting {}
+        val commonTest by getting {
+            dependencies {
+                implementation("io.kotest:kotest-framework-engine:$kotestVersion")
+                implementation("io.kotest:kotest-assertions-core:$kotestVersion")
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+
+        val jvmMain by getting {}
+        val jvmTest by getting {
+            tasks.withType<Test>().configureEach {
+                useJUnitPlatform()
+            }
+            dependencies {
+                implementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+            }
+        }
+
+        val jsMain by getting {}
+        val jsTest by getting {}
+
+        val iosMain by getting {}
+        val iosTest by getting {}
     }
 }
 
@@ -40,13 +68,9 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = jvmVersion.toString()
 }
 
-tasks.withType<Test>().configureEach {
-    useJUnitPlatform()
-}
-
 java {
-    withJavadocJar()
-    withSourcesJar()
+//    withJavadocJar()
+//    withSourcesJar()
 }
 
 nexusStaging {
@@ -63,7 +87,7 @@ publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             artifactId = "kotlin-tree"
-            from(components["java"])
+//            from(components["java"])
             versionMapping {
                 usage("java-api") {
                     fromResolutionOf("runtimeClasspath")
@@ -112,19 +136,48 @@ publishing {
         }
     }
 }
+//
+// tasks.withType<KotlinCompile> {
+//    kotlinOptions.jvmTarget = jvmVersion.toString()
+// }
+
+// tasks.withType<Test>().configureEach {
+//    useJUnitPlatform()
+// }
+
+// tasks.withType<Javadoc>().configureEach {
+//    if (JavaVersion.current().isJava9Compatible) {
+//        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+//    }
+// }
+//
+// tasks.register<Jar>("javadocJar") {
+//    archiveClassifier.set("javadoc")
+//    from(tasks.withType<Javadoc>())
+// }
 
 signing {
     sign(publishing.publications["mavenJava"])
 }
 
-tasks.javadoc {
-    if (JavaVersion.current().isJava9Compatible) {
-        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
-    }
-}
+// tasks.register<Jar>("sourcesJar") {
+//    archiveClassifier.set("sources")
+//    from(sourceSets["main"].allSource)
+// }
+//
+// tasks.register<Jar>("javadocJar") {
+//    archiveClassifier.set("javadoc")
+//    from(tasks.javadoc())
+// }
 
-tasks.register("closeAndReleaseRepositoryIfProductionRelease") {
-    if (!version.toString().endsWith("SNAPSHOT")) {
-        dependsOn("closeAndReleaseRepository")
-    }
-}
+// tasks.javadoc {
+//    if (JavaVersion.current().isJava9Compatible) {
+//        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+//    }
+// }
+//
+// tasks.register("closeAndReleaseRepositoryIfProductionRelease") {
+//    if (!version.toString().endsWith("SNAPSHOT")) {
+//        dependsOn("closeAndReleaseRepository")
+//    }
+// }
