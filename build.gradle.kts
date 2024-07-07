9eb948a83ac40df911aff4874c31c4812771a374
YuitoSato <yuitosato.w@gmail.com>
2024-07-07 14:14:50 +0900

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,52 +1,107 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinMultiplatformPluginWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("multiplatform") version "1.9.20"
+    kotlin("multiplatform") version "1.9.22"
 
     id("org.jlleitschuh.gradle.ktlint") version "11.5.0"
     id("signing")
     id("maven-publish")
     id("io.codearte.nexus-staging") version "0.30.0"
     id("io.kotest.multiplatform") version "5.8.0"
+    id("org.jetbrains.dokka") version "1.9.10"
+    id("com.android.library") version "8.2.0"
+    `maven-publish`
 }
 
 group = "io.github.yuitosato"
-version = "1.5.0"
+version = "1.5.0_SNAPSHOT"
 
 repositories {
     mavenCentral()
+    google()
 }
 
-val kotestVersion = "5.8.0"
+val kotestVersion = "5.9.0"
 
 val jvmVersion = 11
 
 kotlin {
+    applyDefaultHierarchyTemplate()
+    jvm {
+        withSourcesJar()
+    }
     jvmToolchain(jvmVersion)
 
-    jvm() // JVMターゲット
-    js(IR) { // JavaScriptターゲット（IRベース）
+    js(IR) {
         browser()
         nodejs()
     }
-//    androidTarget()
-    iosArm64()
+
+    androidTarget {
+        publishLibraryVariants("release")
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+
+    /* https://kotlinlang.org/docs/native-target-support.html#tier-1 */
+
+    macosX64()
+    macosArm64()
     iosSimulatorArm64()
-//    iosX64("ios") // iOSターゲット（ここではX64のみを例としています）
-    applyDefaultHierarchyTemplate()
+    iosX64()
+
+    /* https://kotlinlang.org/docs/native-target-support.html#tier-2 */
+
+    linuxX64()
+    linuxArm64()
+
+    watchosSimulatorArm64()
+    watchosX64()
+    watchosArm32()
+    watchosArm64()
+
+    tvosSimulatorArm64()
+    tvosX64()
+    tvosArm64()
+
+    iosArm64()
 
     sourceSets {
-        val commonMain by getting {}
-        val commonTest by getting {
-            dependencies {
-                implementation("io.kotest:kotest-framework-engine:$kotestVersion")
-                implementation("io.kotest:kotest-assertions-core:$kotestVersion")
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
+        val commonMain by getting {
+            tasks.withType<KotlinCompile> {
+                kotlinOptions.jvmTarget = jvmVersion.toString()
+            }
+
+            tasks.withType<Javadoc>().configureEach {
+                if (JavaVersion.current().isJava9Compatible) {
+                    (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+                }
+            }
+
+            tasks.register("closeAndReleaseRepositoryIfProductionRelease") {
+                if (!version.toString().endsWith("SNAPSHOT")) {
+                    dependsOn("closeAndReleaseRepository")
+                }
+            }
+
+            tasks.register<Jar>("javadocJar") {
+                archiveClassifier.set("javadoc")
+                from(tasks.withType<Javadoc>())
             }
         }
 
-        val jvmMain by getting {}
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation("io.kotest:kotest-framework-engine:$kotestVersion")
+                implementation("io.kotest:kotest-assertions-core:$kotestVersion")
+            }
+        }
+
         val jvmTest by getting {
             tasks.withType<Test>().configureEach {
                 useJUnitPlatform()
@@ -55,22 +110,11 @@ kotlin {
                 implementation("io.kotest:kotest-runner-junit5:$kotestVersion")
             }
         }
-
-        val jsMain by getting {}
-        val jsTest by getting {}
-
-        val iosMain by getting {}
-        val iosTest by getting {}
     }
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = jvmVersion.toString()
-}
-
-java {
-//    withJavadocJar()
-//    withSourcesJar()
 }
 
 nexusStaging {
@@ -84,10 +128,17 @@ nexusStaging {
 }
 
 publishing {
+    plugins.withType<MavenPublishPlugin> {
+        apply(plugin = "org.gradle.signing")
+
+        plugins.withType<KotlinMultiplatformPluginWrapper> {
+            apply(plugin = "org.jetbrains.dokka")
+        }
+    }
+
     publications {
         create<MavenPublication>("mavenJava") {
             artifactId = "kotlin-tree"
-//            from(components["java"])
             versionMapping {
                 usage("java-api") {
                     fromResolutionOf("runtimeClasspath")
@@ -136,48 +187,19 @@ publishing {
         }
     }
 }
-//
-// tasks.withType<KotlinCompile> {
-//    kotlinOptions.jvmTarget = jvmVersion.toString()
-// }
-
-// tasks.withType<Test>().configureEach {
-//    useJUnitPlatform()
-// }
-
-// tasks.withType<Javadoc>().configureEach {
-//    if (JavaVersion.current().isJava9Compatible) {
-//        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
-//    }
-// }
-//
-// tasks.register<Jar>("javadocJar") {
-//    archiveClassifier.set("javadoc")
-//    from(tasks.withType<Javadoc>())
-// }
 
 signing {
     sign(publishing.publications["mavenJava"])
 }
 
-// tasks.register<Jar>("sourcesJar") {
-//    archiveClassifier.set("sources")
-//    from(sourceSets["main"].allSource)
-// }
-//
-// tasks.register<Jar>("javadocJar") {
-//    archiveClassifier.set("javadoc")
-//    from(tasks.javadoc())
-// }
-
-// tasks.javadoc {
-//    if (JavaVersion.current().isJava9Compatible) {
-//        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
-//    }
-// }
-//
-// tasks.register("closeAndReleaseRepositoryIfProductionRelease") {
-//    if (!version.toString().endsWith("SNAPSHOT")) {
-//        dependsOn("closeAndReleaseRepository")
-//    }
-// }
+android {
+    namespace="io.github.yuitosato.kotlintree"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 28
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
