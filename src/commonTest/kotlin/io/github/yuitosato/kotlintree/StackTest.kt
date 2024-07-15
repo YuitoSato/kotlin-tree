0510bb96b3dd4ff23d99c887d973c49f2df4f169
YuitoSato <yuitosato.w@gmail.com>
2024-07-15 09:06:48 +0900

    ktlint

--- src/commonTest/kotlin/io/github/yuitosato/kotlintree/StackTest.kt
+++ src/commonTest/kotlin/io/github/yuitosato/kotlintree/StackTest.kt
@@ -3,7 +3,7 @@ package io.github.yuitosato.kotlintree
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
-class StackTest: FunSpec({
+class StackTest : FunSpec({
     test("pop() should return the last element and remove it from the stack") {
         val stack = Stack<Int>()
         val size = 10
