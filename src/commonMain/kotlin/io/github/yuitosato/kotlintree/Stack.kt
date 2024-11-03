6d0afba332ab8f94bb2c0434fd47aad4e626c656
YuitoSato <yuitosato.w@gmail.com>
2024-11-03 11:17:37 +0900

    rm unnecessary inline

--- src/commonMain/kotlin/io/github/yuitosato/kotlintree/Stack.kt
+++ src/commonMain/kotlin/io/github/yuitosato/kotlintree/Stack.kt
@@ -3,11 +3,11 @@ package io.github.yuitosato.kotlintree
 internal class Stack<E> {
     private val deque = ArrayDeque<E>()
 
-    inline operator fun plusAssign(item: E) = deque.addLast(item)
+    operator fun plusAssign(item: E) = deque.addLast(item)
 
-    inline fun pop(): E = deque.removeLast()
+    fun pop(): E = deque.removeLast()
 
-    inline fun isNotEmpty() = deque.isNotEmpty()
+    fun isNotEmpty() = deque.isNotEmpty()
 
-    inline fun size() = deque.size
+    fun size() = deque.size
 }
