3c9db062206169e298c94b84090e9f8c4f0f1480
YuitoSato <yuitosato.w@gmail.com>
2024-11-03 11:45:13 +0900

    performance test base

---
+++ src/commonTest/kotlin/io/github/yuitosato/kotlintree/PerformanceTest.kt
@@ -0,0 +1,35 @@
+package io.github.yuitosato.kotlintree
+
+import io.kotest.core.spec.style.FunSpec
+import kotlin.time.Duration
+import kotlin.time.measureTime
+
+class PerformanceTest : FunSpec({
+    test("map").config(enabled = false) {
+        val time1 = measureAverageTimeOf10 {
+            Test.treeNode.map { it + 1 }
+        }
+        println(time1)
+    }
+})
+
+fun measureAverageTimeOf10(f: () -> Unit): Duration {
+    val times = (0 until 10).map {
+        measureTime(f)
+    }
+    return times.reduce { acc, measureDuration -> acc + measureDuration } / 10
+}
+
+object Test {
+    val treeNode = AdjacencyList.of(
+        (0 until 100).flatMap { parentId ->
+            (0 until 10_000).map { selfId ->
+                AdjacencyListItem(parentId, selfId, selfId)
+            }
+        }.plus(AdjacencyListItem(null, 0, 0))
+    ).toTreeNode().treeNodes.first()
+
+    val list = (0..1000000).toList()
+
+    val indices = (0 until 100).toList().map { 0 }
+}
